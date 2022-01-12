b4f65097c5f3def4244f0c0fa6f7de6f80c314c0
ShinJam <nevvjann@gmail.com>
2022-01-12 10:41:08 +0900

    fix(pkg/utils): add redis connection

--- pkg/utils/connection_url_builder.go
+++ pkg/utils/connection_url_builder.go
@@ -23,6 +23,13 @@ func ConnectionURLBuilder(n string) (string, error) {
 			os.Getenv("DB_NAME"),
 			os.Getenv("DB_SSL_MODE"),
 		)
+	case "redis":
+		// URL for Redis connection.
+		url = fmt.Sprintf(
+			"%s:%s",
+			os.Getenv("REDIS_HOST"),
+			os.Getenv("REDIS_PORT"),
+		)
 	case "fiber":
 		// URL for Fiber connection.
 		url = fmt.Sprintf(
