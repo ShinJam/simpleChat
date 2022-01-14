7333cab7b3e3c4612194a9392a0efc74aceed3ea
ShinJam <nevvjann@gmail.com>
2022-01-14 11:58:44 +0900

    feat: implement chat room

--- backend/app/ws/chat_server.go
+++ backend/app/ws/chat_server.go
@@ -5,6 +5,7 @@ type WsServer struct {
 	register   chan *Client
 	unregister chan *Client
 	broadcast  chan []byte
+	rooms      map[*Room]bool
 }
 
 // NewWebsocketServer creates a new WsServer type
@@ -14,6 +15,7 @@ func NewWebsocketServer() *WsServer {
 		register:   make(chan *Client),
 		unregister: make(chan *Client),
 		broadcast:  make(chan []byte),
+		rooms:      make(map[*Room]bool),
 	}
 }
 
@@ -36,15 +38,93 @@ func (server *WsServer) Run() {
 }
 
 func (server *WsServer) registerClient(client *Client) {
+	server.notifyClientJoined(client)
+	server.listOnlineClients(client)
 	server.clients[client] = true
 }
 
 func (server *WsServer) unregisterClient(client *Client) {
-	delete(server.clients, client)
+	if _, ok := server.clients[client]; ok {
+		delete(server.clients, client)
+		server.notifyClientLeft(client)
+	}
+}
+
+func (server *WsServer) notifyClientJoined(client *Client) {
+	message := &Message{
+		Action: UserJoinedAction,
+		Sender: client,
+	}
+
+	server.broadcastToClients(message.encode())
+}
+
+func (server *WsServer) notifyClientLeft(client *Client) {
+	message := &Message{
+		Action: UserLeftAction,
+		Sender: client,
+	}
+
+	server.broadcastToClients(message.encode())
+}
+
+func (server *WsServer) listOnlineClients(client *Client) {
+	for existingClient := range server.clients {
+		message := &Message{
+			Action: UserJoinedAction,
+			Sender: existingClient,
+		}
+		client.send <- message.encode()
+	}
+
 }
 
 func (server *WsServer) broadcastToClients(message []byte) {
 	for client := range server.clients {
 		client.send <- message
 	}
+}
+
+func (server *WsServer) findRoomByName(name string) *Room {
+	var foundRoom *Room
+	for room := range server.rooms {
+		if room.GetName() == name {
+			foundRoom = room
+			break
+		}
+	}
+
+	return foundRoom
+}
+
+func (server *WsServer) findRoomByID(ID string) *Room {
+	var foundRoom *Room
+	for room := range server.rooms {
+		if room.GetId() == ID {
+			foundRoom = room
+			break
+		}
+	}
+
+	return foundRoom
+}
+
+func (server *WsServer) createRoom(name string, private bool) *Room {
+	room := NewRoom(name, private)
+	go room.RunRoom()
+	server.rooms[room] = true
+
+	return room
+}
+
+func (server *WsServer) findClientByID(ID string) *Client {
+	var foundClient *Client
+	for client := range server.clients {
+		if client.ID.String() == ID {
+			foundClient = client
+			break
+		}
+	}
+
+	return foundClient
 }
