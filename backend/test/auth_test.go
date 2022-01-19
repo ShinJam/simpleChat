8c5a1386805ccd045ace0f5177c32fd18f1f43a1
ShinJam <nevvjann@gmail.com>
2022-01-20 01:16:24 +0900

    test: modify to use test database and load fixtures

--- backend/test/auth_test.go
+++ backend/test/auth_test.go
@@ -2,47 +2,42 @@ package test
 
 import (
 	"bytes"
-	"database/sql"
 	"encoding/json"
 	"fmt"
-	"github.com/labstack/gommon/log"
-	"io/ioutil"
-	"net/http/httptest"
-	"regexp"
-	"testing"
-	"time"
-
-	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/gofiber/fiber/v2"
-	"github.com/google/uuid"
 	"github.com/shinjam/simpleChat/app/models"
 	"github.com/shinjam/simpleChat/pkg/routes"
-	"github.com/shinjam/simpleChat/pkg/utils"
 	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"net/http/httptest"
+	"testing"
 )
 
 func TestUserSignIn(t *testing.T) {
-	db, mock := MockQuery()
-	defer func(db *sql.DB) {
-		err := db.Close()
-		if err != nil {
-			log.Error(err)
-		}
-	}(db)
+	//db, mock := MockQuery()
+	//defer func(db *sql.DB) {
+	//	err := db.Close()
+	//	if err != nil {
+	//		log.Error(err)
+	//	}
+	//}(db)
+
+	// Load Fixtures
+	PrepareTestDatabase()
 
 	user := &models.SignIn{
-		Email:    "guest2@test.com",
+		Email:    "guest1@test.com",
 		Password: "1234",
 	}
 
-	// use QuoteMeta func to escape character
-	// reference: https://stackoverflow.com/questions/59652031/sqlmock-is-not-matching-query-but-query-is-identical-and-log-output-shows-the-s
-	query := regexp.QuoteMeta(`SELECT * FROM kuve.user WHERE email = ?`)
-
-	rows := sqlmock.NewRows([]string{"id", "created_at", "updated_at", "email", "password_hash", "user_status", "user_role"}).
-		AddRow(uuid.New(), time.Now(), nil, user.Email, utils.GeneratePassword(user.Password), 1, "user")
-
-	mock.ExpectQuery(query).WithArgs(user.Email).WillReturnRows(rows)
+	//// use QuoteMeta func to escape character
+	//// reference: https://stackoverflow.com/questions/59652031/sqlmock-is-not-matching-query-but-query-is-identical-and-log-output-shows-the-s
+	//query := regexp.QuoteMeta(`SELECT * FROM kuve.user WHERE email = ?`)
+	//
+	//rows := sqlmock.NewRows([]string{"id", "created_at", "updated_at", "email", "password_hash", "user_status", "user_role"}).
+	//	AddRow(uuid.New(), time.Now(), nil, user.Email, utils.GeneratePassword(user.Password), 1, "user")
+	//
+	//mock.ExpectQuery(query).WithArgs(user.Email).WillReturnRows(rows)
 
 	// Define a new Fiber app.
 	app := fiber.New()
@@ -60,9 +55,12 @@ func TestUserSignIn(t *testing.T) {
 	// Verify, that no error occurred, that is not expected
 	if !assert.Equalf(t, false, err != nil, "sign in user") {
 		body, _ := ioutil.ReadAll(resp.Body)
-		fmt.Println(string(body)) // => Hello, World!
+		fmt.Println(string(body))
 	}
 
 	// Verify, if the status code is as expected.
-	assert.Equalf(t, 200, resp.StatusCode, "sign in user")
+	if !assert.Equalf(t, 200, resp.StatusCode, "sign in user") {
+		body, _ := ioutil.ReadAll(resp.Body)
+		fmt.Println(string(body))
+	}
 }
--- backend/test/main_test.go
+++ backend/test/main_test.go
@@ -2,12 +2,34 @@ package test
 
 import (
 	"database/sql"
+	"fmt"
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/lib/pq"
+	"github.com/ory/dockertest/v3"
+	"github.com/ory/dockertest/v3/docker"
+	"github.com/shinjam/simpleChat/pkg/configs"
 	"os"
 	"testing"
+	"time"
 
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/alicebob/miniredis"
+	"github.com/go-testfixtures/testfixtures/v3"
+
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/labstack/gommon/log"
+)
+
+var (
+	fixtures *testfixtures.Loader
+	db       *sql.DB
+)
+
+const (
+	dbName     = "testDB"
+	dbPassword = "1234"
+	dbUser     = "testUser"
 )
 
 func MockQuery() (*sql.DB, sqlmock.Sqlmock) {
@@ -20,6 +42,27 @@ func MockQuery() (*sql.DB, sqlmock.Sqlmock) {
 }
 
 func TestMain(m *testing.M) {
+	// Run redis
+	RunRedis()
+
+	// Run postgres
+	pool, resource := RunTestDatabase()
+
+	// Migrations
+	MigrateUp()
+
+	// Load Fixtures
+	LoadFixtures()
+
+	code := m.Run()
+	// You can't defer this because os.Exit doesn't care for defer
+	if err := pool.Purge(resource); err != nil {
+		log.Fatalf("Could not purge resource: %s", err)
+	}
+	os.Exit(code)
+}
+
+func RunRedis() {
 	mr, err := miniredis.Run()
 	if err != nil {
 		log.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
@@ -29,6 +72,86 @@ func TestMain(m *testing.M) {
 	if err != nil {
 		log.Error(err)
 	}
-	code := m.Run()
-	os.Exit(code)
+}
+
+func RunTestDatabase() (*dockertest.Pool, *dockertest.Resource) {
+	pool, err := dockertest.NewPool("")
+	if err != nil {
+		log.Fatalf("Could not connect to docker: %s", err)
+	}
+
+	// pulls an image, creates a container based on it and runs it
+	resource, err := pool.RunWithOptions(&dockertest.RunOptions{
+		Repository: "postgres",
+		Tag:        "11",
+		Env: []string{
+			"POSTGRES_PASSWORD=" + dbPassword,
+			"POSTGRES_USER=" + dbUser,
+			"POSTGRES_DB=" + dbName,
+			"listen_addresses = '*'",
+		},
+	}, func(config *docker.HostConfig) {
+		// set AutoRemove to true so that stopped container goes away by itself
+		config.AutoRemove = true
+		config.RestartPolicy = docker.RestartPolicy{Name: "no"}
+	})
+	if err != nil {
+		log.Fatalf("Could not start resource: %s", err)
+	}
+
+	hostAndPort := resource.GetHostPort("5432/tcp")
+	databaseUrl := fmt.Sprintf("postgres://%s:%s@%s/%s?sslmode=disable", dbUser, dbPassword, hostAndPort, dbName)
+	os.Setenv("DATABASE_URL", databaseUrl)
+	log.Print("Connecting to database on url: ", databaseUrl)
+
+	resource.Expire(120) // Tell docker to hard kill the container in 120 seconds
+
+	// exponential backoff-retry, because the application in the container might not be ready to accept connections yet
+	pool.MaxWait = 120 * time.Second
+	if err = pool.Retry(func() error {
+		db, err = sql.Open("postgres", databaseUrl)
+		if err != nil {
+			return err
+		}
+		return db.Ping()
+	}); err != nil {
+		log.Fatalf("Could not connect to docker: %s", err)
+	}
+
+	return pool, resource
+}
+
+func MigrateUp() {
+	fileURL := fmt.Sprint("file://" + configs.RootDir + "/platform/migrations")
+	log.Print(fileURL)
+	m, err := migrate.New(
+		fileURL,
+		os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := m.Up(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func LoadFixtures() {
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Error(err)
+	}
+	fixtures, err = testfixtures.New(
+		testfixtures.Database(db),            // You database connection
+		testfixtures.Dialect("postgres"),     // Available: "postgresql", "timescaledb", "mysql", "mariadb", "sqlite" and "sqlserver"
+		testfixtures.Directory("./fixtures"), // The directory containing the YAML files
+	)
+	if err != nil {
+		log.Error(err)
+	}
+}
+
+func PrepareTestDatabase() {
+	if err := fixtures.Load(); err != nil {
+		log.Error(err)
+	}
 }
