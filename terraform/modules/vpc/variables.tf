17f2214de2135dc73dbbf00a743cf5897191d448
ShinJam <nevvjann@gmail.com>
2022-01-24 19:05:01 +0900

    build(terraform): add vpc and module

---
+++ terraform/modules/vpc/variables.tf
@@ -0,0 +1,36 @@
+# vpc
+variable "name" {
+  description = "Name to be used on all the resources as identifier"
+  default     = ""
+}
+
+variable "cidr" {
+  description = "The CIDR block for the VPC. Default value is a valid CIDR, but not acceptable by AWS and should be overriden"
+  default     = "0.0.0.0/0"
+}
+
+variable "azs" {
+  description = "A list of availability zones in the region"
+  type        = list(string)
+  default     = []
+}
+
+variable "private_subnets" {
+  description = "A list of private subnets inside the VPC"
+  type        = list(string)
+  default     = []
+}
+
+variable "public_subnets" {
+  description = "A list of public subnets inside the VPC"
+  type        = list(string)
+  default     = []
+}
+
+# variable "database_subnets" {
+#   description = "A list of database subnets inside the VPC"
+#   type        = list(string)
+#   default     = []
+# }
+
+variable "tags" {}
