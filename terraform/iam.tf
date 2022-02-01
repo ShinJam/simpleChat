43bf75cd8cae7a24f65f5e863278906e2b25bd3b
ShinJam <nevvjann@gmail.com>
2022-02-01 20:36:16 +0900

    build(jenkins): updates jenkinsfile with cd

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,24 +1,32 @@
-resource "aws_iam_instance_profile" "jenkins" {
-  name = "jenkins-ec2-profile"
-  role = aws_iam_role.jenkins_role.name
-  tags = local.common_tags
-}
-
-resource "aws_iam_role" "jenkins_role" {
-  name = "jenkins-ec2-role"
+resource "aws_iam_role" "default_role" {
+  name = "default-ec2-role"
   tags = local.common_tags
 
   assume_role_policy = data.aws_iam_policy_document.default.json
 }
 
+# Jenkins
+resource "aws_iam_instance_profile" "jenkins" {
+  name = "jenkins-ec2-profile"
+  role = aws_iam_role.default_role.name
+  tags = local.common_tags
+}
+
 resource "aws_iam_role_policy" "main" {
   name   = "jenkins-ec2-policy"
-  role   = aws_iam_role.jenkins_role.id
-  policy = data.aws_iam_policy_document.get_params_by_path.json
+  role   = aws_iam_role.default_role.id
+  policy = data.aws_iam_policy_document.ssm.json
 }
 
 
 resource "aws_iam_role_policy_attachment" "jenkins_ecr_policy" {
-  role       = aws_iam_role.jenkins_role.name
+  role       = aws_iam_role.default_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser"
+}
+
+# API Server
+resource "aws_iam_instance_profile" "api-server" {
+  name = "api-server-ec2-profile"
+  role = aws_iam_role.default_role.name
+  tags = local.common_tags
 }
