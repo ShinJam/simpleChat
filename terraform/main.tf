5307d28a00e69a76794c92dd1cfcb0659ec1e01d
ShinJam <nevvjann@gmail.com>
2022-02-02 17:06:09 +0900

    build(jenkins): implement api-server up

--- terraform/main.tf
+++ terraform/main.tf
@@ -99,6 +99,8 @@ module "api-server" {
 
   sg_ids    = tolist([module.sg.ec2_security_group_id, ])
   subnet_id = element(module.vpc.private_subnets, 0)
+  associate_public_ip_address = false
+  iam_instance_profile        = aws_iam_instance_profile.api-server.name
 
   user_data = data.template_file.api_userdata.rendered
 
